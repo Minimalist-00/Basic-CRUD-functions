a5f83e89c852524d46f264e497ad93e7dc4731aa
Minimalist-00 <kouki4122@icloud.com>
2023-07-09 13:59:04 +0900

    http only: true

--- router/router.go
+++ router/router.go
@@ -33,7 +33,7 @@ func NewRouter(uc controller.IUserController, qc controller.IQuestController) *e
 	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
 		CookiePath:     "/",
 		CookieDomain:   os.Getenv("API_DOMAIN"),
-		CookieHTTPOnly: false,
+		CookieHTTPOnly: true,
 		CookieSameSite: http.SameSiteNoneMode, //TODO: フロントエンドとの通信にはSameSiteNoneModeを使う
 		// CookieSameSite: http.SameSiteDefaultMode, //TODO: Postmanでのテスト用
 		CookieMaxAge: 60 * 60, // csrfトークンの有効期限（秒）.
